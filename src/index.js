261d1524448a2f800e7c3c11996853e76292604b
pmfoysal <pmfoysal@gmail.com>
2023-03-29 12:40:33 +0600

    App UI Styles Added

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import App from './app/app';
+import './styles/index.css';
+import './styles/custom.css';
 import ReactDOM from 'react-dom/client';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
