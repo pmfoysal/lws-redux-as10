6a637f41e442833529c39439c18284acfc6068b9
pmfoysal <pmfoysal@gmail.com>
2023-03-31 11:53:08 +0600

    Video Edit Page Component Added

--- src/pages/admin/videoEdit.jsx
+++ src/pages/admin/videoEdit.jsx
@@ -1,11 +1,17 @@
 import { Fragment } from 'react';
 import Head from '../../components/head';
+import VideoForm from '../../components/videoForm';
 
 export default function VideoEdit() {
    return (
       <Fragment>
          <Head title={`Edit Video - `} desc={``} />
-         <section>VideoEdit</section>
+         <section className='py-6'>
+            <div className='mx-auto max-w-7xl px-5 py-6 lg:px-0'>
+               <h1 className='mt-8 text-2xl font-bold text-center'>Edit a Video</h1>
+               <VideoForm mode='edit' />
+            </div>
+         </section>
       </Fragment>
    );
 }
