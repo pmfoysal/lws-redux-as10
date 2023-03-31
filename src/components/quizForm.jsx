87d5a5199953bd76a49426edbac0660819d12dea
pmfoysal <pmfoysal@gmail.com>
2023-03-31 15:44:32 +0600

    Quiz Edit Page Component Added

--- src/components/quizForm.jsx
+++ src/components/quizForm.jsx
@@ -54,7 +54,14 @@ export default function QuizForm({ mode }) {
 
    return (
       <form className='add-form'>
-         <input type='text' autoComplete required className='login-input rounded-md' placeholder='Video Title' />
+         <input
+            type='text'
+            autoComplete
+            required
+            className='login-input rounded-md'
+            placeholder='Video Title'
+            readOnly={mode === 'edit'}
+         />
          {questions.map((qItem, qIndex) => (
             <div key={`question-${qIndex}`} className={`question ${opened.includes(qIndex) ? 'active' : ''}`}>
                <textarea
@@ -99,21 +106,30 @@ export default function QuizForm({ mode }) {
                </div>
             </div>
          ))}
-         <div className='input-group'>
-            <button
-               type='button'
-               onClick={handleAddQuestion}
-               className='mt-3 group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-violet-600 hover:bg-violet-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-violet-500'
-            >
-               Add Question
-            </button>
+         {mode === 'add' ? (
+            <div className='input-group'>
+               <button
+                  type='button'
+                  onClick={handleAddQuestion}
+                  className='mt-3 group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-violet-600 hover:bg-violet-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-violet-500'
+               >
+                  Add Question
+               </button>
+               <button
+                  type='submit'
+                  className='mt-3 group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-violet-600 hover:bg-violet-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-violet-500'
+               >
+                  Submit
+               </button>
+            </div>
+         ) : (
             <button
                type='submit'
                className='mt-3 group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-violet-600 hover:bg-violet-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-violet-500'
             >
                Submit
             </button>
-         </div>
+         )}
       </form>
    );
 }
