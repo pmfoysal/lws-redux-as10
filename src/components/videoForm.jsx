f7f852de8c3a34af5d05458042134eedf04d0a35
pmfoysal <pmfoysal@gmail.com>
2023-04-04 00:15:10 +0600

    Admin Videos Edit Functionality Added

--- src/components/videoForm.jsx
+++ src/components/videoForm.jsx
@@ -1,22 +1,96 @@
+import urlToId from '../utilities/urlToId';
+import { useEffect, useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import { useEditVideoMutation, useGetVideoQuery } from '../redux/features/videos/enhancer';
+
 export default function VideoForm({ mode }) {
+   const navigate = useNavigate();
+   const { id_title } = useParams();
+   const [editVideo, editVideoApi] = useEditVideoMutation();
+   const { data: video } = useGetVideoQuery(urlToId(id_title), { skip: !id_title || mode !== 'edit' });
+
+   const [url, setUrl] = useState('');
+   const [title, setTitle] = useState('');
+   const [views, setViews] = useState('');
+   const [duration, setDuration] = useState('');
+   const [description, setDescription] = useState('');
+
+   function handleSubmit(event) {
+      event.preventDefault();
+      const payload = { title, description, url, views, duration };
+      if (mode === 'add') {
+         payload.createdAt = new Date().toISOString();
+      } else if (mode === 'edit') {
+         payload.id = video.id;
+         editVideo(payload).then(() => {
+            navigate('/admin/videos');
+         });
+      }
+   }
+
+   useEffect(() => {
+      if (video?.id !== undefined && mode === 'edit') {
+         setUrl(video.url);
+         setTitle(video.title);
+         setViews(video.views);
+         setDuration(video.duration);
+         setDescription(video.description);
+      }
+   }, [video, mode]);
+
    return (
-      <form className='add-form'>
-         <input type='text' autoComplete required className='login-input rounded-md' placeholder='Video Title' />
-         <textarea
+      <form className='add-form' onSubmit={handleSubmit}>
+         <input
+            required
             type='text'
             autoComplete
+            value={title}
+            placeholder='Video Title'
+            className='login-input rounded-md'
+            onChange={e => setTitle(e.target.value)}
+         />
+         <textarea
             required
             rows={6}
-            className='login-input rounded-md'
+            type='text'
+            autoComplete
+            value={description}
             placeholder='Video Description'
+            className='login-input rounded-md'
+            onChange={e => setDescription(e.target.value)}
          />
-         <input type='url' autoComplete required className='login-input rounded-md' placeholder='Video Url' />
+         <input
+            required
+            type='url'
+            value={url}
+            autoComplete
+            placeholder='Video Url'
+            className='login-input rounded-md'
+            onChange={e => setUrl(e.target.value)}
+         />
          <div className='input-group'>
-            <input type='text' autoComplete required className='login-input rounded-md' placeholder='Total Views' />
-            <input type='text' autoComplete required className='login-input rounded-md' placeholder='Total Duration' />
+            <input
+               required
+               type='text'
+               autoComplete
+               value={views}
+               placeholder='Total Views'
+               className='login-input rounded-md'
+               onChange={e => setViews(e.target.value)}
+            />
+            <input
+               required
+               type='text'
+               autoComplete
+               value={duration}
+               placeholder='Total Duration'
+               className='login-input rounded-md'
+               onChange={e => setDuration(e.target.value)}
+            />
          </div>
          <button
             type='submit'
+            disabled={editVideoApi.isLoading}
             className='mt-3 group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-violet-600 hover:bg-violet-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-violet-500'
          >
             {mode === 'add' ? '+ Add' : 'Update'}
--- src/pages/admin/videoEdit.jsx
+++ src/pages/admin/videoEdit.jsx
@@ -1,11 +1,19 @@
 import { Fragment } from 'react';
 import Head from '../../components/head';
+import { useParams } from 'react-router-dom';
+import urlToId from '../../utilities/urlToId';
 import VideoForm from '../../components/videoForm';
+import PageLoader from '../../components/pageLoader';
+import { useGetVideoQuery } from '../../redux/features/videos/enhancer';
 
 export default function VideoEdit() {
+   const { id_title } = useParams();
+   const { data: video, isLoading } = useGetVideoQuery(urlToId(id_title), { skip: !id_title });
+
    return (
       <Fragment>
-         <Head title={`Edit Video - `} desc={``} />
+         <Head title={`Edit Video - ${video?.title}`} desc={`${video?.description}`} />
+         {isLoading ? <PageLoader /> : null}
          <section className='py-6'>
             <div className='mx-auto max-w-7xl px-5 py-6 lg:px-0'>
                <h1 className='mt-8 text-2xl font-bold text-center'>Edit a Video</h1>
