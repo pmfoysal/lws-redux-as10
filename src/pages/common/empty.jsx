f4a6bba2ab7dc61215c74eaeb400fedefe801b56
pmfoysal <pmfoysal@gmail.com>
2023-04-05 14:06:24 +0600

    Admin Quiz List Page No Data Page Added

--- src/pages/common/empty.jsx
+++ src/pages/common/empty.jsx
@@ -1,11 +1,11 @@
 import { Fragment } from 'react';
 import Head from '../../components/head';
 
-export default function Empty({ text }) {
+export default function Empty({ text, scope = 'outer' }) {
    return (
       <Fragment>
          <Head title={text} />
-         <section className='py-6 page-empty'>
+         <section className={`py-6 page-empty ${scope}`}>
             <div className='mx-auto max-w-7xl px-5 lg:px-0 container-empty'>
                <h1 className='text-2xl font-bold text-empty'>{text}</h1>
             </div>
