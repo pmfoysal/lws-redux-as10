210927ba6df31f81107a602a7ffb96cb6e4cd998
pmfoysal <pmfoysal@gmail.com>
2023-04-05 14:18:10 +0600

    Admin Assignment Edit Page No Data Page Added

--- src/pages/admin/assignmentEdit.jsx
+++ src/pages/admin/assignmentEdit.jsx
@@ -1,4 +1,5 @@
 import { Fragment } from 'react';
+import Empty from '../common/empty';
 import Head from '../../components/head';
 import { useParams } from 'react-router-dom';
 import urlToId from '../../utilities/urlToId';
@@ -8,15 +9,16 @@ import { useGetAssignmentQuery } from '../../redux/features/assignments/enhancer
 
 export default function AssignmentEdit() {
    const { id_title } = useParams();
-   const assignmentApi = useGetAssignmentQuery(urlToId(id_title));
+   const { data: assignment, isLoading } = useGetAssignmentQuery(urlToId(id_title), { skip: !id_title });
+
+   if (id_title && !isLoading && assignment?.id === undefined) {
+      return <Empty text='No assignment found with this parameters!' />;
+   }
 
    return (
       <Fragment>
-         <Head
-            title={`Edit Assignment - ${assignmentApi.data?.title}`}
-            desc={`Assignment of "${assignmentApi.data?.video_title}"`}
-         />
-         {assignmentApi.isLoading ? <PageLoader /> : null}
+         <Head title={`Edit Assignment - ${assignment?.title}`} desc={`Assignment of "${assignment?.video_title}"`} />
+         {isLoading ? <PageLoader /> : null}
          <section className='py-6'>
             <div className='mx-auto max-w-7xl px-5 py-6 lg:px-0'>
                <h1 className='mt-8 text-2xl font-bold text-center'>Edit a Assignment</h1>
--- src/pages/admin/quizEdit.jsx
+++ src/pages/admin/quizEdit.jsx
@@ -9,16 +9,16 @@ import { useGetQuizQuery } from '../../redux/features/quizzes/enhancer';
 
 export default function QuizEdit() {
    const { id_title } = useParams();
-   const quizApi = useGetQuizQuery(urlToId(id_title), { skip: !id_title });
+   const { data: quiz, isLoading } = useGetQuizQuery(urlToId(id_title), { skip: !id_title });
 
-   if (!quizApi.isLoading && quizApi.data?.id === undefined) {
+   if (id_title && !isLoading && quiz?.id === undefined) {
       return <Empty text='No quiz found with this parameters!' />;
    }
 
    return (
       <Fragment>
-         <Head title={`Edit Quiz - ${quizApi.data?.question}`} desc={`Quiz of "${quizApi.data?.video_title}"`} />
-         {quizApi.isLoading ? <PageLoader /> : null}
+         <Head title={`Edit Quiz - ${quiz?.question}`} desc={`Quiz of "${quiz?.video_title}"`} />
+         {isLoading ? <PageLoader /> : null}
          <section className='py-6'>
             <div className='mx-auto max-w-7xl px-5 py-6 lg:px-0'>
                <h1 className='mt-8 text-2xl font-bold text-center'>Edit a Quiz</h1>
